4e257c355ea369e436c952c633655a9670e9fedb
yohei tanaka <oceanyoh@gmail.com>
2018-10-05 23:36:46 +0900

    Hello world

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            Hello World
+          </h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Reactを試してみる
           </p>
           <a
             className="App-link"
